f4351eb738962544b1d86f98206cb0a1ed244008
Slaytekk <alienhuntsgames@gmail.com>
2024-04-23 00:36:37 -0500

    removed code until I figure this out

--- project_programs/data_structure_program/src/main.rs
+++ project_programs/data_structure_program/src/main.rs
@@ -5,28 +5,11 @@ struct VideoGame {
     genre: String,
 }
 
-// impl VideoGame {
-//     fn get_game_name(&self) -> String {
-//         String::from(self.name)
-//     }
-// }
-
 struct Library {
     // keep a list of games
-    games: Vec<VideoGame>,
 }
 
-// impl Library {
-//     fn findGame(&self, game_name: String) -> usize {
-//         let mut i = 0;
 
-//         while self.games[i].get_game_name() != game_name {
-//             i += 1;
-//         }
-
-//         return i;
-//     }
-// }
 
 fn main() {
     let mut my_vec: Vec<String> = Vec::new();
