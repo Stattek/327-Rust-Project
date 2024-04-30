4fc8eee064b91a38aa52483d11b11e4ba81d6378
Jake Ozer <jakobozer1@gmail.com>
2024-04-30 16:34:34 -0500

    it dont work ;(

--- project_programs/program3/src/main.rs
+++ project_programs/program3/src/main.rs
@@ -4,6 +4,7 @@
 use std::io;
 
 fn main() {
+    //starting menu
     println!("---------------------------------------------------------------");
     println!("Welcome to the Nutri-Diet Incorporated's BRAND NEW Diet Planner");
     println!("---------------------------------------------------------------");
@@ -28,5 +29,18 @@ fn main() {
         println!("Input is out of range. Please enter a number between 1 and 3.");
     }
 
-    println!("\nOkay, now enter in the current foods you eat on a regular basis, line by line ->");
+    //user inputs foods they eat
+    let mut food_list: Vec<String> = Vec::new();
+    println!("\nOkay, now enter in the current foods you eat on a regular basis, line by line. enter STOP to see results ->");
+    let mut input = String::new();
+    while input.trim() != "STOP" {
+        io::stdin().read_line(&mut input).expect("Failed to read line"); 
+        println!("DEBUG {}", input);
+        food_list.push(input.clone());
+        println!("\nEnter another current food you eat on a regular basis, enter STOP to see results ->");
+        input.clear();
+    }
+
+    println!("{:?}", food_list);
+    print!("end program debug");
 }
