11fbe9a28f7c190571adaf0598ee0d431bfe06b5
stattek <alienhuntsgames@gmail.com>
2024-04-30 17:10:02 -0500

    fixed the not equals operation on input

--- project_programs/program3/src/main.rs
+++ project_programs/program3/src/main.rs
@@ -14,17 +14,23 @@ fn main() {
     println!("3. The Supernova Salmon Slim Down (Pescatarian Diet)");
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line"); 
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    //shadowing input to be an i32 and hold our value
     let input: i32 = input.trim().parse().expect("Please enter a number");
 
     if input >= 1 && input <= 3 {
         match input {
-            1=>println!("You entered: {}, THE PALEO CAVEMAN MUSCLE BUILDER EXTREME", input),
-            2=>println!("You entered: {}, IF LOOKS COULD KALE", input),
-            3=>println!("You entered: {}, THE SUPERNOVA SALMON SLIM DOWN", input),
-            _=>println!("input not found."),
+            1 => println!(
+                "You entered: {}, THE PALEO CAVEMAN MUSCLE BUILDER EXTREME",
+                input
+            ),
+            2 => println!("You entered: {}, IF LOOKS COULD KALE", input),
+            3 => println!("You entered: {}, THE SUPERNOVA SALMON SLIM DOWN", input),
+            _ => println!("input not found."),
         }
-       
     } else {
         println!("Input is out of range. Please enter a number between 1 and 3.");
     }
@@ -33,12 +39,20 @@ fn main() {
     let mut food_list: Vec<String> = Vec::new();
     println!("\nOkay, now enter in the current foods you eat on a regular basis, line by line. enter STOP to see results ->");
     let mut input = String::new();
-    while input.trim().to_string() != "STOP".to_string() {
-        io::stdin().read_line(&mut input).expect("Failed to read line"); 
-        println!("DEBUG {}", input);
+
+    while input.trim() != "STOP" {
+        // reset the value of input to avoid errors
+        input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
         food_list.push(input.clone());
-        println!("\nEnter another current food you eat on a regular basis, enter STOP to see results ->");
-        input.clear();
+
+        println!(
+            "\nEnter another current food you eat on a regular basis, enter STOP to see results ->"
+        );
     }
 
     println!("{:?}", food_list);
